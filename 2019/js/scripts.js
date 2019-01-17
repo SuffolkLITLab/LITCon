c528c9612ea923cf45a0fa36d2126d06dbdbc310
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-01-17 15:56:26 -0500

    updates

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("Thank you for making our inaugural conference a success.");
+  $('#msg').html("<b>Save the date (April 8th, 2019). <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>Apply to present a rapid-fire talk here</a>.</b>");
   $('#msg').show();
   return true;
 }
