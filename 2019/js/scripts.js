fb91ae28e505159691d0efc1fdc966a89f7079cb
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-02-15 16:36:59 -0500

    hold off on registration link

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<b>Registration now open. <a href='https://portalpro.suffolk.edu/forms3/Forms?v:=439'>Register here!!!</a>. Then <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>apply to present a rapid-fire talk here</a>.</b>");
+  $('#msg').html("<b>Save the date (April 8th, 2019). <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>Apply to present a rapid-fire talk here</a>.</b><!-- Registration now open. <a href='https://portalpro.suffolk.edu/forms3/Forms?v:=439'>Register here!!!</a>. Then <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>apply to present a rapid-fire talk here</a>.</b>-->");
   $('#msg').show();
   return true;
 }
