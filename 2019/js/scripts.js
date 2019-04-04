eef9264a095eb7ce16d87e985e3ab828a2b74011
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-04-04 16:35:24 -0400

    bot down speakers up

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<!--<b>Save the date (April 8th, 2019). <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>Apply to present a rapid-fire talk here</a>.</b>--><b><a href='https://portalpro.suffolk.edu/forms3/Forms?v:=439' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Registration');\">Register now ($25)!</a> Registration closes April 1. <a href='https://suffolklitlab.org/slack/' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Join Slack');\">Join our Slack team</a>.</b>");
+  $('#msg').html("<!--<b>Save the date (April 8th, 2019). <a href='https://docs.google.com/forms/d/e/1FAIpQLScoRWp4BlJ4n3lMXIBFpr1p1tBVO_mFsdeA9PM9rI3IZK4dZA/viewform' traget='_blank'>Apply to present a rapid-fire talk here</a>.</b><b><a href='https://portalpro.suffolk.edu/forms3/Forms?v:=439' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Registration');\">Register now ($25)!</a> Registration closes April 1.--> We can't wait to see you all on the 8th. <a href='https://suffolklitlab.org/slack/' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Join Slack');\">Join our Slack team</a> to start the conversation early.</b>");
   $('#msg').show();
   return true;
 }
