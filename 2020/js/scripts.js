102a8d1de8a20bb7d0bfdf658e8d0a1507272ad7
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-10-17 16:58:18 -0400

    2020 updates

---
+++ 2020/js/scripts.js
@@ -0,0 +1,5 @@
+function msg() {
+  $('#msg').html("<b>Save the date (March 30th, 2020). <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_bQ2ANLEINOrM28aHLVu2DnGZuYbiETyGsD2Z6xhHxT7qmg/viewform' target='_blank'>Apply to present a rapid-fire talk here</a>.</b><!--Thank you to everyone who helped make #LITCon2019 happen. <a href='https://suffolklitlab.org/slack/' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Join Slack');\">Join our Slack team</a> to keep the conversation going.--></b>");
+  $('#msg').show();
+  return true;
+}
