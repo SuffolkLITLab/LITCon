551b47b073e7a3be0ce6d2b09c5673124325bd31
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-11-12 10:50:27 -0500

    tweaks

--- 2020/js/scripts.js
+++ 2020/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<b><a href='https://web.cvent.com/event/cf22c986-4b48-454f-9dfb-3545fe93f86d/register' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Rigister Now!</a> (March 30th, 2020) <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_bQ2ANLEINOrM28aHLVu2DnGZuYbiETyGsD2Z6xhHxT7qmg/viewform' target='_blank'>Apply to present a rapid-fire talk</a>.</b><!--Thank you to everyone who helped make #LITCon2019 happen. <a href='https://suffolklitlab.org/slack/' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Join Slack');\">Join our Slack team</a> to keep the conversation going.--></b>");
+  $('#msg').html("<b><a href='https://web.cvent.com/event/cf22c986-4b48-454f-9dfb-3545fe93f86d/summary' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Rigister Now!</a> (March 30th, 2020) <a href='https://docs.google.com/forms/d/e/1FAIpQLSd_bQ2ANLEINOrM28aHLVu2DnGZuYbiETyGsD2Z6xhHxT7qmg/viewform' target='_blank'>Apply to present a rapid-fire talk</a>.</b><!--Thank you to everyone who helped make #LITCon2019 happen. <a href='https://suffolklitlab.org/slack/' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Join Slack');\">Join our Slack team</a> to keep the conversation going.--></b>");
   $('#msg').show();
   return true;
 }
