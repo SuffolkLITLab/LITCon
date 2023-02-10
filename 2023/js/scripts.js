147c30258d028605ab9fb1ef4f29106b46380c02
Quinten Steenhuis <qsteenhuis@gmail.com>
2023-02-10 12:29:49 -0500

    Other typos

--- 2023/js/scripts.js
+++ 2023/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<a href='https://forms.gle/WwG2H6DeZn4e1oah6' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Signup for updates!</a> (April 3th, 2023) <a href='https://forms.gle/A2Hhq4rjpVHqcWb57' target='_blank'>Apply to present a rapid-fire talk</a>.</b><!--Thank you to everyone who helped make #LITCon2023 happen.");
+  $('#msg').html("<a href='https://forms.gle/WwG2H6DeZn4e1oah6' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Sign up for updates!</a> (April 3rd, 2023) <a href='https://forms.gle/A2Hhq4rjpVHqcWb57' target='_blank'>Apply to present a rapid-fire talk</a>.</b><!--Thank you to everyone who helped make #LITCon2023 happen.");
   $('#msg').show();
   return true;
 }
