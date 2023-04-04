568e6abb2e55db22924a03d6b20e8c58ef69b0fc
David Colarusso <colarusso@users.noreply.github.com>
2023-04-04 10:16:37 -0400

    youtube link

--- 2023/js/scripts.js
+++ 2023/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<a href='https://www.youtube.com/playlist?list=PLy6i9GFGw5GwuczR7PkFoaKDk2ic78loP' target='_blank'>We're streaming on YouTube!</a> <!--<a href='https://forms.gle/gVP6Xq1C63ZK2cDC9' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Register Now!</a> (April 3rd, 2023) <a href='https://forms.gle/A2Hhq4rjpVHqcWb57' target='_blank'>Apply to present a rapid-fire talk</a>.</b> Thank you to everyone who helped make #LITCon2023 happen.-->");
+  $('#msg').html("<a href='https://www.youtube.com/playlist?list=PLy6i9GFGw5GwuczR7PkFoaKDk2ic78loP' target='_blank'>Watch it all on YouTube!</a> <!--<a href='https://forms.gle/gVP6Xq1C63ZK2cDC9' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Register Now!</a> (April 3rd, 2023) <a href='https://forms.gle/A2Hhq4rjpVHqcWb57' target='_blank'>Apply to present a rapid-fire talk</a>.</b> Thank you to everyone who helped make #LITCon2023 happen.-->");
   $('#msg').show();
   return true;
 }
