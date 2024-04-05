7514f38e9a9ae32488bd60455c8f62f0b4d7a8f4
David Colarusso <colarusso@users.noreply.github.com>
2024-04-05 01:04:32 -0400

    update speakers page and agenda

--- 2024/js/scripts.js
+++ 2024/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<!-- <a href='' target='_blank'>Get notified</a> when registation opens.--> <!--<a href='' target='_blank'>We're streaming on YouTube!</a>--> <a href='https://forms.gle/V8zZsdzkPNq8ixp7A' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">Register Now!</a> (April 8rd, 2024) <a href='https://forms.gle/oya2XVYuVCVNrRby9'>Apply</a> to present a rapid-fire talk.</b> <!--Thank you to everyone who helped make #LITCon2024 happen.-->");
+  $('#msg').html("<!-- <a href='' target='_blank'>Get notified</a> when registation opens.--> <!--<a href='' target='_blank'>We're streaming on YouTube!</a>--> The in-person event is sold out, but you can still <a href='https://forms.gle/V8zZsdzkPNq8ixp7A' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">register</a> for the online event. (April 8rd, 2024) <!--<a href='https://forms.gle/oya2XVYuVCVNrRby9'>Apply</a> to present a rapid-fire talk.</b> Thank you to everyone who helped make #LITCon2024 happen.-->");
   $('#msg').show();
   return true;
 }
