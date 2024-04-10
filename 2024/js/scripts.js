e84472980c03167ecb0b48f6b16a9389f91c5215
David Colarusso <colarusso@users.noreply.github.com>
2024-04-10 12:44:37 -0400

    link target

--- 2024/js/scripts.js
+++ 2024/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<!-- <a href='' target='_blank'>Get notified</a> when registation opens.--> <!--<a href='' target='_blank'>We're streaming on YouTube!</a>--> <!-- <a href='https://forms.gle/V8zZsdzkPNq8ixp7A' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">register</a> for the online event. (April 8rd, 2024) <!--<a href='https://forms.gle/oya2XVYuVCVNrRby9'>Apply</a> to present a rapid-fire talk.</b>--> Thank you to everyone who helped make #LITCon2024 happen. Video of the day's events can be found <a href='https://www.youtube.com/playlist?list=PLy6i9GFGw5GyhlV-WqtqYRhdqjieR03Tp'>here</a>.");
+  $('#msg').html("<!-- <a href='' target='_blank'>Get notified</a> when registation opens.--> <!--<a href='' target='_blank'>We're streaming on YouTube!</a>--> <!-- <a href='https://forms.gle/V8zZsdzkPNq8ixp7A' target='_blank' onClick=\"ga('send', 'event', 'clicks', 'click', 'Register top');\">register</a> for the online event. (April 8rd, 2024) <!--<a href='https://forms.gle/oya2XVYuVCVNrRby9'>Apply</a> to present a rapid-fire talk.</b>--> Thank you to everyone who helped make #LITCon2024 happen. Video of the day's events can be found <a href='https://www.youtube.com/playlist?list=PLy6i9GFGw5GyhlV-WqtqYRhdqjieR03Tp' target='_blank'>here</a>.");
   $('#msg').show();
   return true;
 }
